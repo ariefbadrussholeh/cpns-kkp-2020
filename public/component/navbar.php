7354e40332f526b5ed7b215b04e4ea3075c38b7c
Arief Badrus Sholeh <arifbadrus08@gmail.com>
2022-12-18 13:37:18 +0700

    feat: finish biodata menu

--- public/component/navbar.php
+++ public/component/navbar.php
@@ -1,17 +1,17 @@
 <nav id="navbar">
-  <div class="menu active">
+  <div class="menu active" id="dashboard">
     <img src="./public/img/home.svg" alt="" />
     <p>Dashboard</p>
   </div>
-  <div class="menu">
+  <div class="menu" id="biodata">
     <img src="./public/img/users.svg" alt="" />
     <p>Biodata</p>
   </div>
-  <div class="menu">
+  <div class="menu" id="dokumen">
     <img src="./public/img/folder.svg" alt="" />
     <p>Dokumen</p>
   </div>
-  <div class="menu">
+  <div class="menu" id="logout">
     <img src="./public/img/power.svg" alt="" />
     <p>Logout</p>
   </div>
--- public/js/navbar-toggle.js
+++ public/js/navbar-toggle.js
@@ -7,3 +7,20 @@ check.addEventListener("click", function () {
     navbar.style.display = "none";
   }
 });
+
+const dashboard = document.getElementById("dashboard");
+dashboard.addEventListener("click", function () {
+  window.location.href = "./dashboard.php";
+});
+const biodata = document.getElementById("biodata");
+biodata.addEventListener("click", function () {
+  window.location.href = "./biodata.php";
+});
+const dokumen = document.getElementById("dokumen");
+dokumen.addEventListener("click", function () {
+  window.location.href = "./dokumen.php";
+});
+const logout = document.getElementById("logout");
+logout.addEventListener("click", function () {
+  window.location.href = "./proses-logout.php";
+});
