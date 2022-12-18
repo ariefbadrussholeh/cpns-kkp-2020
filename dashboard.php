dfb659c970addecb9ad058d0db8c79781ba2c51e
Arief Badrus Sholeh <arifbadrus08@gmail.com>
2022-12-18 10:35:36 +0700

    feat: finish dashboard view

--- dashboard.php
+++ dashboard.php
@@ -16,104 +16,106 @@
   </head>
   <body>
     <?php include './public/component/header.php'?>
-    <?php include './public/component/navbar.php'?>
-    <main>
-      <div class="box">
-        <div class="box-group">
-          <div class="timeline">
-            <div class="title">
-              <div class="number box-verified">1</div>
-              <h1 class="verified">Registrasi</h1>
-            </div>
-            <div class="group1">
-              <p>
-                <b>Jadwal</b> : 13 Desember 2022 - 13.30 WIB s.d 30 Desember 2022 - 23.59 WIB
-                <br> <br>
-                Waktu Indonesia Barat (UTC+07:00)
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="group2">
-          <img src="./public/img/calendar.svg" alt="" width="15px">  <b>Dilakukan Pada</b> : 17 Desember 2022 - 11.30 WIB
-        </div>
-      </div>
-      <div class="box">
-        <div class="box-group">
-          <div class="timeline">
-            <div class="title">
-              <div class="number box-waiting">2</div>
-              <h1 class="waiting">Biodata</h1>
-            </div>
-            <div class="group1">
-              <p>
-                <b>Jadwal</b> : 13 Desember 2022 - 13.30 WIB s.d 30 Desember 2022 - 23.59 WIB
-                <br> <br>
-                Waktu Indonesia Barat (UTC+07:00)
-              </p>
+    <div class="container">
+      <?php include './public/component/navbar.php'?>
+      <main>
+        <div class="box">
+          <div class="box-group">
+            <div class="timeline">
+              <div class="title">
+                <div class="number box-verified">1</div>
+                <h1 class="verified">Registrasi</h1>
+              </div>
+              <div class="group1">
+                <p>
+                  <b>Jadwal</b> : 13 Desember 2022 - 13.30 WIB s.d 30 Desember 2022 - 23.59 WIB
+                  <br> <br>
+                  Waktu Indonesia Barat (UTC+07:00)
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="group2">
-          <img src="./public/img/calendar.svg" alt="" width="15px">  <b>Dilakukan Pada</b> : 18 Desember 2022 - 11.30 WIB
-        </div>
-      </div>
-      <div class="box">
-        <div class="box-group">
-          <div class="timeline">
-            <div class="title">
-              <div class="number box-unfinished">3</div>
-              <h1 class="unfinished">Seleksi Berkas</h1>
-            </div>
-            <div class="group1">
-              <p>
-                <b>Jadwal</b> : 13 Desember 2022 - 13.30 WIB s.d 30 Desember 2022 - 23.59 WIB
-                <br> <br>
-                Waktu Indonesia Barat (UTC+07:00)
-              </p>
-            </div>
+          <div class="group2">
+            <img src="./public/img/calendar.svg" alt="" width="15px">  <b>Dilakukan Pada</b> : 17 Desember 2022 - 11.30 WIB
           </div>
         </div>
-        <div class="group2">
-          <img src="./public/img/calendar.svg" alt="" width="15px">  <b>Dilakukan Pada</b> : -
-        </div>
-      </div>
-      <div class="box">
-        <div class="box-group">
-          <div class="timeline">
-            <div class="title">
-              <div class="number box-unfinished">4</div>
-              <h1 class="unfinished">Lolos</h1>
-            </div>
-            <div class="group1">
-              <p>Selamat anda dinyatakan lolos seleksi pemberkasan</p>
-              <button disabled>Unduh Kartu Peserta</button>
+        <div class="box">
+          <div class="box-group">
+            <div class="timeline">
+              <div class="title">
+                <div class="number box-waiting">2</div>
+                <h1 class="waiting">Biodata</h1>
+              </div>
+              <div class="group1">
+                <p>
+                  <b>Jadwal</b> : 13 Desember 2022 - 13.30 WIB s.d 30 Desember 2022 - 23.59 WIB
+                  <br> <br>
+                  Waktu Indonesia Barat (UTC+07:00)
+                </p>
+              </div>
             </div>
           </div>
+          <div class="group2">
+            <img src="./public/img/calendar.svg" alt="" width="15px">  <b>Dilakukan Pada</b> : 18 Desember 2022 - 11.30 WIB
+          </div>
         </div>
-        <div class="group2">
-          <img src="./public/img/calendar.svg" alt="" width="15px">  <b>Dilakukan Pada</b> : -
+        <div class="box">
+          <div class="box-group">
+            <div class="timeline">
+              <div class="title">
+                <div class="number box-unfinished">3</div>
+                <h1 class="unfinished">Seleksi Berkas</h1>
+              </div>
+              <div class="group1">
+                <p>
+                  <b>Jadwal</b> : 13 Desember 2022 - 13.30 WIB s.d 30 Desember 2022 - 23.59 WIB
+                  <br> <br>
+                  Waktu Indonesia Barat (UTC+07:00)
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="group2">
+            <img src="./public/img/calendar.svg" alt="" width="15px">  <b>Dilakukan Pada</b> : -
+          </div>
         </div>
-      </div>
-      <div class="box" style="padding: 24px; background-color: #eaeaea;">
-        <div class="list">
-          <div style="width: 25px; height: 25px; background-color: #69aa63; border-radius: 50px; margin-right: 16px;"></div>
-          <p>Telah dilakukan dan terverifikasi</p>
+        <div class="box">
+          <div class="box-group">
+            <div class="timeline">
+              <div class="title">
+                <div class="number box-unfinished">4</div>
+                <h1 class="unfinished">Lolos</h1>
+              </div>
+              <div class="group1">
+                <p style="text-align: center;">Selamat anda dinyatakan lolos seleksi pemberkasan</p>
+                <button disabled>Unduh Kartu Peserta</button>
+              </div>
+            </div>
+          </div>
+          <div class="group2">
+            <img src="./public/img/calendar.svg" alt="" width="15px">  <b>Dilakukan Pada</b> : -
+          </div>
         </div>
-        <div class="list">
-          <div style="width: 25px; height: 25px; background-color: #dfc435; border-radius: 50px; margin-right: 16px;"></div>
-          <p>Telah dilakukan dan menunggu validasi</p>
+        <div class="box" style="padding: 24px; background-color: #eaeaea;">
+          <div class="list">
+            <div style="width: 25px; height: 25px; background-color: #69aa63; border-radius: 50px; margin-right: 16px;"></div>
+            <p>Telah dilakukan dan terverifikasi</p>
+          </div>
+          <div class="list">
+            <div style="width: 25px; height: 25px; background-color: #dfc435; border-radius: 50px; margin-right: 16px;"></div>
+            <p>Telah dilakukan dan menunggu validasi</p>
+          </div>
+          <div class="list">
+            <div style="width: 25px; height: 25px; background-color: #d9d9d9; border-radius: 50px; margin-right: 16px;"></div>
+            <p>Belum melakukan proses pendaftaran</p>
+          </div>
         </div>
-        <div class="list">
-          <div style="width: 25px; height: 25px; background-color: #d9d9d9; border-radius: 50px; margin-right: 16px;"></div>
-          <p>Belum melakukan proses pendaftaran</p>
+        <div class="box" style="padding: 24px; background-color: #f3e8ad;">
+          Demi menghindari penyalahgunaan data pribadi, mohon untuk tidak memberikna data apapun selain yang diisikan pada Sistem Seleksi Pegawai Kementrian Kelautan dan Perikanan Prov. Jatim
         </div>
-      </div>
-      <div class="box" style="padding: 24px; background-color: #f3e8ad;">
-        Demi menghindari penyalahgunaan data pribadi, mohon untuk tidak memberikna data apapun selain yang diisikan pada Sistem Seleksi Pegawai Kementrian Kelautan dan Perikanan Prov. Jatim
-      </div>
-    </main>
-    <!-- script -->
+      </main>
+    </div>
+    <!-- Javascript -->
     <script src="./public/js/navbar-toggle.js"></script>
   </body>
 </html>
